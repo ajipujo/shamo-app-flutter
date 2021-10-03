0e07ec18055946fdf369eb0308f542fa133d2826
Aji Pujo Hardiyanto <ajipujohardiyanto@gmail.com>
2021-10-03 21:28:27 +0700

    Chat detail product preview

--- lib/pages/detail_chat_page.dart
+++ lib/pages/detail_chat_page.dart
@@ -118,10 +118,15 @@ class DetailChatPage extends StatelessWidget {
           ChatBubble(
             isSender: true,
             text: 'Hi, This item is still available?',
+            hasProduct: true,
           ),
           ChatBubble(
             isSender: false,
             text: 'Good night, This item is only available in size 42 and 43',
+          ),
+          ChatBubble(
+            isSender: true,
+            text: 'Owww, it suits me very well',
           ),
         ],
       );
